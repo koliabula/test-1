90222c62492ebc2cfdfa0ba4f0af5ac5d8f87087
Bulla Kolia <koliakolan@gmail.com>
2022-10-16 01:00:23 +0300

    программа работает корректно

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿void PrintArray (string [] ar){
+    for (int i = 0; i < ar.Length; i++)
+    {
+        Console.Write($"{ar[i]} ");
+    }
+    Console.WriteLine();
+}
+
+String [] Sample3 (string [] str){
+    String [] StrResult = new string [str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3){
+            StrResult[i] = str[i];
+        }
+    }
+    return StrResult;
+}
+
+String [] StrArray = {"text","tex", "te", "t"};
+PrintArray(Sample3(StrArray));
+
+
